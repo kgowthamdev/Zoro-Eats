99a89606ba2f8acea8c4f05ac890db08a0c96ab8
gowtham <kgowtham612@gmail.com>
2024-02-10 22:28:22 +0530

    configured react js and tailwind css

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client";
+
+const heading=<p className="text-5xl text-red-900">zoro Eat's name is inspired from the anime onepice. tailwind CSS is properly installed and react js is properly configured</p>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
